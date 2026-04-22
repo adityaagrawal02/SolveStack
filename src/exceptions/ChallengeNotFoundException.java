f7032b5ad63753b574b9b1168024badba6086e16
Aditya <agrawal02adi@gmail.com>
2026-04-22 10:08:55 +0530

    added custom exception classes (SolveStackException, SubmissionDeadlineException, UserNotFoundException, ChallengeNotFoundException)

---
+++ src/exceptions/ChallengeNotFoundException.java
@@ -0,0 +1,27 @@
+package exceptions;
+
+/**
+ * Thrown when a challenge cannot be found in the ChallengeService registry.
+ * Example: a developer tries to submit to a challenge ID that no longer exists.
+ */
+public class ChallengeNotFoundException extends SolveStackException {
+
+    private final String challengeId;
+
+    public ChallengeNotFoundException(String challengeId) {
+        super("Challenge not found with ID: '" + challengeId + "'", "CHALLENGE_NOT_FOUND");
+        this.challengeId = challengeId;
+    }
+
+    public ChallengeNotFoundException(String challengeId, String customMessage) {
+        super(customMessage, "CHALLENGE_NOT_FOUND");
+        this.challengeId = challengeId;
+    }
+
+    /**
+     * Returns the challenge ID that was searched for.
+     */
+    public String getChallengeId() {
+        return challengeId;
+    }
+}
